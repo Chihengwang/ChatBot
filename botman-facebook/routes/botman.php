db0b88934f17c181a5f6653c2446fbe2431209e7
chihengwang <chihengwang@outlook.com>
2017-12-10 17:32:03 +0800

    init

---
+++ botman-facebook/routes/botman.php
@@ -0,0 +1,18 @@
+<?php
+use App\Http\Controllers\BotManController;
+
+$botman = resolve('botman');
+
+
+$botman->hears('Hi', function ($bot) {
+    $bot->reply('Hello!');
+});
+$botman->hears('Fuck', function ($bot) {
+    $bot->reply('Hello!');
+});
+$botman->hears('call me {name}', function ($bot, $name) {
+    $bot->reply('Your name is: '.$name);
+});
+
+$botman->hears('Start conversation', BotManController::class.'@startConversation');
+
